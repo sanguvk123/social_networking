9d822c20f944d09ca523119552e4b810cd86dbeb
SANGAMESH K <sanguvk123@gmail.com>
2020-02-21 03:06:47 +0530

    logout

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
+
 
 @app.route('/')
 @app.route('/index')
@@ -19,7 +22,21 @@ def home():
 
     return render_template('index.html', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
